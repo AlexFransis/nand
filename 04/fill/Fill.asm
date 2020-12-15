118e10fc4a5391ae395c58a896f78d08420d3cbc
Alex <alex.fransis@gmail.com>
2020-12-15 00:40:38 -0500

    implement fill with conditinal check

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,35 +1,61 @@
-@SCREEN
-M=!M
-A=A+1
-M=!M
-A=A+1
-M=!M
-@SCREEN
-M=!M
+@state
+M=0 // 0 == white; 1 == black
 
-//@32
-//@256
-//M=A
-//
-//(LOOP1)
-//	@256
-//	MD=A-1
-//	@END
-//	D;JEQ
-//	@32
-//	M=A
-//	(LOOP2)
-//		@SCREEN
-//		M=!M
-//		@32
-//		MD=M-1
-//		@LOOP1
-//		D;JEQ
-//		@SCREEN
-//		A=A+1
-//		@LOOP2
-//		0;JMP
-//	
-//(END)
-//	@END
-//	0;JMP
+(KBDLOOP)
+@8191 // 256 x 32 - 1; row * col
+M=0 // reset word pointer
+
+@24576 // kdb address
+D=M
+@SHOULDFILLWHITE
+D;JEQ
+@SHOULDFILLBLACK
+D;JNE
+
+(SHOULDFILLBLACK)
+@state
+D=M-1
+@KBDLOOP 
+D;JEQ // state == black ? goto kbdloop : fill black
+@FILLBLACK
+0;JMP
+
+(SHOULDFILLWHITE)
+@state
+D=M
+@KBDLOOP
+D;JEQ // state == white ? goto kbdloop : fill white
+@FILLWHITE
+0;JMP
+
+(FILLBLACK)
+@8191
+D=M
+@SCREEN
+A=A+D
+M=!M
+@8191
+MD=M+1
+D=D-A // curr word - total words < 0 ? goto loop : continue
+@FILLBLACK
+D;JLE
+@state
+M=1 // black
+@KBDLOOP
+0;JMP
+
+(FILLWHITE)
+@8191
+D=M
+@SCREEN
+A=A+D
+M=0
+@8191
+MD=M+1
+D=D-A // curr word - total words < 0 ? goto loop : continue
+@FILLWHITE
+D;JLE
+@state
+M=0 // white
+@KBDLOOP
+0;JMP
