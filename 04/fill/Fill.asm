121f8a9cf0166dbb3afebb4f9efe18feae0d3ddf
Alex <alex.fransis@gmail.com>
2020-12-15 22:41:30 -0500

    small optimization

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -2,9 +2,6 @@
 M=0 // x0000 == white; xFFFF == black
 
 (KBDLOOP)
-@8191 // 256 x 32 - 1; row * col
-M=0 // reset word pointer
-
 @KBD
 D=M
 @state
@@ -19,18 +16,22 @@ D=D&M
 @KBDLOOP 
 D;JNE
 
-	(FILL)
-	@8191
-	D=M
-	@SCREEN
-	A=A+D
-	M=!M
-	@8191
-	MD=M+1
-	D=D-A
-	@FILL // curr word - total words < 0 ? goto fill : goto kbdloop
-	D;JLE
-	@state
-	M=!M // toggle state
-	@KBDLOOP
-	0;JMP
+@8191 // 256 x 32 - 1; row * col
+M=0 // reset word pointer
+
+(FILL)
+@8191
+D=M
+@SCREEN
+A=A+D
+M=!M
+@8191
+MD=M+1
+D=D-A
+@FILL // curr word - total words < 0 ? goto fill : goto kbdloop
+D;JLE
+
+@state
+M=!M // toggle state
+@KBDLOOP
+0;JMP
