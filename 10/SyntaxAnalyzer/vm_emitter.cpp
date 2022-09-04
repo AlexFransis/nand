9f10ff18165b8441f7ca25b3ba1c689c8092f2a9
gh0st <alex.fransis@gmail.com>
2022-09-03 23:21:32 -0400

    format_push_pop() uses switch case instead of ifs

--- 10/SyntaxAnalyzer/vm_emitter.cpp
+++ 10/SyntaxAnalyzer/vm_emitter.cpp
@@ -2,44 +2,50 @@
 #include <cassert>
 #include <string>
 
-const std::string PUSH = "push";
-const std::string POP = "pop";
-
-const std::string ARGUMENT = "argument";
-const std::string LOCAL = "local";
-const std::string STATIC = "static";
-const std::string CONSTANT = "constant";
-const std::string THIS = "this";
-const std::string THAT = "that";
-const std::string POINTER = "pointer";
-const std::string TEMP = "temp";
-
-const std::string ADD = "add";
-const std::string SUB = "sub";
-const std::string NEG = "neg";
-const std::string EQ = "eq";
-const std::string GT = "gt";
-const std::string LT = "lt";
-const std::string AND = "and";
-const std::string OR = "or";
-const std::string NOT = "not";
-
 
 std::string format_push_pop(const PUSH_POP &push_pop, const SEGMENT &segment, int i)
 {
         std::string result;
 
-        if (push_pop == PUSH_POP::PUSH) result += "push ";
-        if (push_pop == PUSH_POP::POP) result += "pop ";
+        switch (push_pop) {
+        case PUSH_POP::PUSH :
+                result += "push ";
+                break;
+        case PUSH_POP::POP :
+                result += "pop ";
+                break;
+        default:
+                break;
+        }
 
-        if (segment == SEGMENT::ARGUMENT) result += "argument ";
-        if (segment == SEGMENT::LOCAL) result += "local ";
-        if (segment == SEGMENT::STATIC) result += "static ";
-        if (segment == SEGMENT::CONSTANT) result += "constant ";
-        if (segment == SEGMENT::THIS) result += "this ";
-        if (segment == SEGMENT::THAT) result += "that ";
-        if (segment == SEGMENT::POINTER) result += "pointer ";
-        if (segment == SEGMENT::TEMP) result += "temp ";
+        switch (segment) {
+        case SEGMENT::ARGUMENT:
+                result += "argument ";
+                break;
+        case SEGMENT::LOCAL:
+                result += "local ";
+                break;
+        case SEGMENT::STATIC:
+                result += "static ";
+                break;
+        case SEGMENT::CONSTANT:
+                result += "constant ";
+                break;
+        case SEGMENT::THIS:
+                result += "this ";
+                break;
+        case SEGMENT::THAT:
+                result += "that ";
+                break;
+        case SEGMENT::POINTER:
+                result += "pointer ";
+                break;
+        case SEGMENT::TEMP:
+                result += "temp ";
+                break;
+        default:
+                break;
+        }
 
         result += std::to_string(i);
 
